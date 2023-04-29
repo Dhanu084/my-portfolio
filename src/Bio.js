77e55dd8abad54ac7b491740878aabd6a5016957
dhanu <dhanuu08@gmail.com>
2023-04-29 18:54:23 +0530

    added links to icons

--- src/Bio.js
+++ src/Bio.js
@@ -2,6 +2,24 @@ import React from "react";
 import { FaLinkedin, FaGithub } from "react-icons/fa";
 import { SiLeetcode } from "react-icons/si";
 
+const commonClassName =
+  "text-3xl cursor-pointer hover:shadow-gray-300 shadow-lg";
+
+const icons = {
+  linkedin: {
+    url: "https://www.linkedin.com/in/dhanush-kiran-60915217b",
+    icon: <FaLinkedin className={commonClassName} />
+  },
+  github: {
+    url: "https://github.com/Dhanu084",
+    icon: <FaGithub className={commonClassName} />
+  },
+  leetcode: {
+    url: "https://leetcode.com/dhanu084",
+    icon: <SiLeetcode className={commonClassName} />
+  }
+};
+
 const Bio = () => (
   <section
     id='bio'
@@ -11,15 +29,16 @@ const Bio = () => (
     <h1 className='text-5xl mt-8'>Dhanush Kiran</h1>
     <h2 className='text-2xl mt-2'>Software Engineer</h2>
     <ul className='w-48 flex mt-8 justify-between'>
-      <li>
-        <FaLinkedin className='text-3xl cursor-pointer hover:shadow-indigo-300 shadow-lg' />
-      </li>
-      <li>
-        <FaGithub className='text-3xl cursor-pointer hover:shadow-indigo-300 shadow-xl' />
-      </li>
-      <li>
-        <SiLeetcode className='text-3xl cursor-pointer hover:shadow-indigo-300 shadow-lg' />
-      </li>
+      {Object.entries(icons).map(([key, value]) => {
+        const { url, icon } = value;
+        return (
+          <li key={key}>
+            <a href={url} target='_blank'>
+              {icon}
+            </a>
+          </li>
+        );
+      })}
     </ul>
   </section>
 );
