793afb350d17de8b82d1d905e76e4aab49d6dcf2
Dhanush Kiran <54932658+Dhanu084@users.noreply.github.com>
2023-09-24 12:40:28 +0530

    revamped experience section with animations

--- src/Components/Experiences.js
+++ src/Components/Experiences.js
@@ -1,18 +1,28 @@
 import React, { useRef } from "react";
 import useIntersectionObserver from "../hooks/useIntersectionObserver";
+import { color, motion } from "framer-motion";
+import { Card, Timeline } from "antd";
 
 const experienceList = [
+  {
+    companyName: "Tekion Corp",
+    designation: "Software Engineer II",
+    duration: "Present",
+    techStack: "ReactJs",
+    brandColor: "text-teal-500"
+  },
   {
     companyName: "Avalara",
     designation: "Software Engineer",
     duration: "3 years",
-    techStack: "ReactJs, NodeJs, Redux, Python, .NET, Terraform, AWS",
+    techStack: "ReactJs, NodeJs, Redux, Python, AWS",
     brandColor: "text-orange-400"
   },
   {
     companyName: "Infosys",
     designation: "Systems Engineer",
     duration: "1.5 years",
+    techStack: "IBM tools",
     brandColor: "text-blue-400"
   },
   {
@@ -24,55 +34,57 @@ const experienceList = [
   }
 ];
 
+const cardVariance = {
+  hidden: { opacity: 1, translateX: "-40vw" },
+  visible: {
+    opacity: 1,
+    translateX: 0,
+    transition: {
+      delayChildren: 0.3,
+      staggerChildren: 0.2
+    }
+  }
+};
+
+const createTimeLineItems = () => {
+  const items = experienceList.map((exp) => ({
+    // label: exp.duration,
+    color: exp.companyName === experienceList[0].companyName ? "green" : "gray",
+    children: (
+      <motion.div
+        variants={cardVariance}
+        initial='hidden'
+        whileInView='visible'
+      >
+        <Card bordered={true} style={{ width: "auto" }}>
+          <h4 className={exp.brandColor}>{exp.companyName}</h4>
+          <p>{exp.designation}</p>
+          <p>
+            <small>{exp.duration}</small>
+          </p>
+          <p>{exp.techStack}</p>
+        </Card>
+      </motion.div>
+    )
+  }));
+  return items;
+};
+
 const Experiences = () => {
   const observable = useRef(null);
-  const { shouldAnimate } = useIntersectionObserver(observable);
+  // const { shouldAnimate } = useIntersectionObserver(observable);
 
   return (
     <section
       id='experience'
-      className='sm:h-screen sm:w-screen flex flex-col justify-around'
-      ref={observable}
+      className='h-auto w-screen flex flex-col justify-around '
     >
-      <h1 className='text-5xl text-center m-12 sm:m-0 mt-12'>Experiences</h1>
-      <div className='mt-4 h-[1200px]  sm:mt-0 sm:h-4/5 flex flex-col sm:flex-row justify-around items-center  sm:px-8 '>
-        {experienceList.map(
-          ({ companyName, duration, techStack, designation, brandColor }) => (
-            <div
-              className={`mx-auto mt-4 sm:mt-0  h-3/5 sm:h-3/5 lg:h-3/4 w-3/4 sm:w-80 shadow-lg ${
-                shouldAnimate ? "animate-bounce" : ""
-              } dark:border-2 border-white`}
-              key={companyName}
-            >
-              <div className='relative h-2/5 sm:h-2/5 w-full bg-black  flex justify-center items-center text-xl dark:bg-red-400'>
-                <div
-                  className={`absolute bg-white text-4xl h-40 w-40 text-center -bottom-20 rounded-full flex justify-center items-center shadow-lg ${brandColor} dark:border-2 border-black`}
-                >
-                  {companyName}
-                </div>
-              </div>
-              <div className='h-2/5 sm:h-3/5 p-4 pt-20 mt-2'>
-                <ul children='h-full flex flex-col justify-between'>
-                  <li>
-                    <span className='font-bold'>Role: </span>{" "}
-                    <span>{designation}</span>
-                  </li>
-                  <li>
-                    <span className='font-bold'>Duration: </span>
-                    <span>{duration}</span>
-                  </li>
-                  {techStack && (
-                    <li>
-                      <span className='font-bold'>TechStack: </span>
-                      {techStack}{" "}
-                    </li>
-                  )}
-                </ul>
-              </div>
-            </div>
-          )
-        )}
+      <h1 className='text-5xl text-center m-12 sm:m-0 mt-2'>Experiences</h1>
+      {/* <div className='mt-4 h-[6000px]  sm:mt-0 sm:h-4/5 flex flex-col sm:flex-row justify-around items-center  sm:px-8 '> */}
+      <div className='h-3/4 w-3/4 md:mt-8 mx-auto '>
+        <Timeline items={createTimeLineItems()} />
       </div>
+      {/* </div> */}
     </section>
   );
 };
